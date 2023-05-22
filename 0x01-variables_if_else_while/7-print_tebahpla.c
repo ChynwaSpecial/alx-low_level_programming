d9cc73cb0ba9c806d472e67e0d605dfae1edc04f
ChynwaSpecial <chinenyeifeoma26@gmail.com>
2023-05-22 00:50:05 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 /**
  * main - Entry point
+ * Description: A program to print the lowercase alphabet in reverse order
  * Result: 0 (Success)
  */
 
 int main(void)
 {
 	char l;
-	for ( l = 'z'; l >= 'a'; l--)
+
+	for (l = 'z'; l >= 'a'; l--)
 	{
 		putchar(l);
 	}
 	putchar('\n');
 
-	return (0);
+	return (0); success
 }
