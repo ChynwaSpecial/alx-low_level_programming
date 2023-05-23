43d793793f78a521a39371fc9920e1bf51b11650
ChynwaSpecial <chinenyeifeoma26@gmail.com>
2023-05-22 19:31:46 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-#include <unistd.head>
+#include <unistd.h>
 /**
- * main - A program that prints a line to the standard error
+ * main - Entry point
  * Return: 1
  */
+
 int main(void)
 {
-write(1, " and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59);
-return (1);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
