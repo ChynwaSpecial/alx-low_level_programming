89db6a09776008a57ef947abb2461b40f8dcd842
ChynwaSpecial <chinenyeifeoma26@gmail.com>
2023-05-23 21:27:45 -0700

    3-islower.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,19 +2,18 @@
 /**
  * main - Entry point
  * Description: Print alphabet 10 times
- * Return: 0 (Success)
  */
 
 void print_alphabet_x10(void)
 {
 	int m;
 	char n;
-
+i
 	for (m = 0; m <= 10; m++)
 	{
 		for (n = 'a'; n <= 'z'; n++)
 			_putchar(n);
-	}
 	_putchar('\n');
+	}
 
 }
