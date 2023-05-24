382bd6a9893f0384fa28821f7b0c3383411458a1
ChynwaSpecial <chinenyeifeoma26@gmail.com>
2023-05-23 21:42:01 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,13 @@
 #include "main.h"
+
 /**
  *   print_alphabet_x10 - Print alphabet 10 times
  */
-
 void print_alphabet_x10(void)
 {
 	int m;
 	char n;
-i
+
 	for (m = 0; m <= 10; m++)
 	{
 		for (n = 'a'; n <= 'z'; n++)
