71feff59e40e49eec3eb36934e88d090b5823fd3
ChynwaSpecial <chinenyeifeoma26@gmail.com>
2023-05-23 20:59:14 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: Print alphabet 10 times
+ * Return: 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int m;
+	char n;
+
+	for (m = 0; m <= 9; m++)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+			_putchar(n);
+	}
+	_putchar('\n');
+
+}
