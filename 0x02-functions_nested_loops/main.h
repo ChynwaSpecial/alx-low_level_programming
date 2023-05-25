45997307186d7e412b2f1a10670169d29a3c522c
ChynwaSpecial <chinenyeifeoma26@gmail.com>
2023-05-24 18:09:06 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,15 +2,6 @@
 #define MAIN_H
 
 int _putchar(char c);
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -19,6 +10,7 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
+void times_table(void);
 
 
 
