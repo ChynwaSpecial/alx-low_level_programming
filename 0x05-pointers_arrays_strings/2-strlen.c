b18a5a594272196ccd28a94c99c56e99aa2d4a93
ChynwaSpecial <chinenyeifeoma26@gmail.com>
2023-05-29 18:55:33 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - Returns the length of a string
+ * @s: String
+ * Return: Length
+ */
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (*s != '\0')
+	{
+		l++;
+		s++;
+	}
+	return (l);
+
+}
