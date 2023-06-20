1a0e4f54b2e1b44d77b831cb2e1001448f4f1a7c
ChynwaSpecial <chinenyeifeoma26@gmail.com>
2023-06-20 04:13:56 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+* struct dog - a dog's basic info
+* @name: First member
+* @age: Second member
+* @owner: Third member
+* Return 0 (Success)
+*/
+
 struct dog
 {
 	char *name;
