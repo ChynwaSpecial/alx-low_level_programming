865eef52ee2ac90d220ea5b07d63aa68759867fc
ChynwaSpecial <chinenyeifeoma26@gmail.com>
2023-07-01 01:42:23 -0700

    101-wildcmp

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * is_prime_number - evaluate prime number
+ * @n: input number
+ * Return 1 if n is prime number, otherwise 0
+ */
+int is_prime_number(int n)
+{
+	int i = 2;
+
+	if (n < 2)
+		return (0);
+	if (n == 2)
+	return (1);
+}
+
