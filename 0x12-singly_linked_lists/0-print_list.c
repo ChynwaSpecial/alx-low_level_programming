9640ba0dc9cfbf4d71e3fca6952036061bf728d7
ChynwaSpecial <chinenyeifeoma26@gmail.com>
2023-07-04 00:42:47 -0700

    print_list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,7 +5,7 @@
 /**
  * print_list - prints all the elements of a list_t list
  * @h: list_t list pointer
- * Return: number of nodes
+ * Return: number of nodes in h
  */
 size_t print_list(const list_t *h)
 {
