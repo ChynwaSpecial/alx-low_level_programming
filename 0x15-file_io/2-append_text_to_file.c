f439bb355d2ae66e4f7a16c5bd8cef91b5084460
ChynwaSpecial <chinenyeifeoma26@gmail.com>
2023-07-10 19:28:53 -0700

    https://intranet.alxswe.com/projects/234#:~:text=File%3A%202%2D-,append_text_to_file,-.chttps://intranet.alxswe.com/projects/234#:~:text=File%3A%202%2D-,append_text_to_file,-.c

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - function that appends text at the end of a file
+ * @filename: name of file
+ * @text_content: content written to file
+ * Return: 1 if the file exists, otherwise -1
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int ch;
+	int alpha;
+	int nWR;
+
+	if (filename == NULL)
+		return (-1);
+
+	ch = open(filename, O_WRONLY | O_APPEND);
+
+	if (ch == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (alpha = 0; text_content[alpha]; alpha++)
+			;
+
+		nWR = write(ch, text_content, alpha);
+
+		if (nWR == -1)
+			return (-1);
+	}
+
+	close(ch);
+
+	return (1);
+}
