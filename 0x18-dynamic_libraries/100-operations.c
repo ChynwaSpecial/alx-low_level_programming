3f7dfa346c5b3b863d326290c1c200c871294778
ChynwaSpecial <chinenyeifeoma26@gmail.com>
2023-08-25 18:02:54 -0700

    Test

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(void)
+{
+	int add(int i, int j)
+	{
+		return (i + j);
+	}
+	int sub(int i, int j)
+	{
+		return (i - j);
+	}
+	int mul(int i, int j)
+	{
+		return (i * j);
+	}
+	int div(int i, int j)
+	{
+		return (i / j);
+	}
+	int mod(int i, int j)
+	{
+		return (i % j);
+	}
+}
+
